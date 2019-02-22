64417ee6000da9bfbd9614c6f5114a62dfd7765a
Leroy357 <1003301648@qq.com>
2019-02-21 20:11:46 -0800

    try run auto copy

--- grasp/autocopy.sh
+++ grasp/autocopy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp empty.xml $GRASPIT/worlds/
 cp -r panda $GRASPIT/models/robots
-#python script/scale_wrl_file.py # run this to generate ycb objects
-cp script/meshes/* $GRASPIT/models/objects/
+python scripts/scale_wrl_file.py # run this to generate ycb objects
+cp mesh/* $GRASPIT/models/objects/
 cp src/searchStateImpl.cpp $GRASPIT/src/EGPlanner/
 cp src/searchStateImpl.h $GRASPIT/include/graspit/EGPlanner/
