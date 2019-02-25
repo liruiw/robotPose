06c218e36c5059fb69d1cdb2c754de4814b8c1ab
yux <yux@nvidia.com>
2019-02-25 15:06:30 -0800

    add walls

--- grasp/scripts/compute_world_poses_real.py
+++ grasp/scripts/compute_world_poses_real.py
@@ -238,6 +238,44 @@ if __name__ == '__main__':
             pose_table[6] = points[2, :].min() - 0.8
             print(pose_table)
 
+        pose_table_obstacle = pose_table.copy()
+        pose_table_obstacle[4] += 0.9
+        pose_table_obstacle[5] += 0.5
+        pose_table_obstacle[6] += 0.3
+
+        # wall left
+        pose_wall_left = np.zeros((7,), dtype=np.float32)
+        pose_wall_left[0] = 1
+        pose_wall_left[1] = 0
+        pose_wall_left[2] = 0
+        pose_wall_left[3] = 0
+        if len(cls_indexes) > 0:
+            pose_wall_left[4] = np.mean(points[0, :])
+            pose_wall_left[5] = np.min(points[1, :]) - 0.2
+            pose_wall_left[6] = np.mean(points[2, :])
+
+        # wall right
+        pose_wall_right = np.zeros((7,), dtype=np.float32)
+        pose_wall_right[0] = 1
+        pose_wall_right[1] = 0
+        pose_wall_right[2] = 0
+        pose_wall_right[3] = 0
+        if len(cls_indexes) > 0:
+            pose_wall_right[4] = np.mean(points[0, :])
+            pose_wall_right[5] = np.max(points[1, :]) + 0.2
+            pose_wall_right[6] = np.mean(points[2, :])
+
+        # wall top
+        pose_wall_top = np.zeros((7,), dtype=np.float32)
+        pose_wall_top[0] = 1
+        pose_wall_top[1] = 0
+        pose_wall_top[2] = 0
+        pose_wall_top[3] = 0
+        if len(cls_indexes) > 0:
+            pose_wall_top[4] = np.mean(points[0, :])
+            pose_wall_top[5] = np.mean(points[1, :])
+            pose_wall_top[6] = np.max(points[2, :]) + 0.5
+
         # publish poses
         for i in range(len(cls_indexes)):
             # object pose
@@ -247,9 +285,9 @@ if __name__ == '__main__':
             RT = np.dot(RT_base, RT)
 
             if ycb_classes[i][3] == '_':
-                source_frame = prefix + ycb_classes[cls_indexes[i]][4:] + suffix
+                source_frame = prefix + ycb_classes[cls_indexes[i]][4:]
             else:
-                source_frame = prefix + ycb_classes[cls_indexes[i]] + suffix
+                source_frame = prefix + ycb_classes[cls_indexes[i]]
             br.sendTransform(RT[:3, 3], ros_quat(mat2quat(RT[:3, :3])), rospy.Time.now(), source_frame, world_frame)
 
             # hand pose
@@ -263,6 +301,18 @@ if __name__ == '__main__':
             # table pose
             source_frame = 'table' + suffix
             br.sendTransform(pose_table[4:7], ros_quat(pose_table[:4]), rospy.Time.now(), source_frame, world_frame)
+
+            source_frame = 'table_obstacle'
+            br.sendTransform(pose_table_obstacle[4:7], ros_quat(pose_table_obstacle[:4]), rospy.Time.now(), source_frame, world_frame)
+
+            source_frame = 'wall_left'
+            br.sendTransform(pose_wall_left[4:7], ros_quat(pose_wall_left[:4]), rospy.Time.now(), source_frame, world_frame)
+
+            source_frame = 'wall_right'
+            br.sendTransform(pose_wall_right[4:7], ros_quat(pose_wall_right[:4]), rospy.Time.now(), source_frame, world_frame)
+
+            source_frame = 'wall_top'
+            br.sendTransform(pose_wall_top[4:7], ros_quat(pose_wall_top[:4]), rospy.Time.now(), source_frame, world_frame)
 
             # base in camera
             RT = np.linalg.inv(RT_base)
